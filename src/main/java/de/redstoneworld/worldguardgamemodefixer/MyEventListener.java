aa621e154e61a5fe7a8c5dc257553b9d83eb3982
RedstoneFuture <redstonefuture@redstoneworld.de>
2022-04-04 22:12:08 +0200

    adding gamemode change for server join

--- src/main/java/de/redstoneworld/worldguardgamemodefixer/MyEventListener.java
+++ src/main/java/de/redstoneworld/worldguardgamemodefixer/MyEventListener.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class MyEventListener implements Listener {
@@ -21,6 +22,27 @@ public class MyEventListener implements Listener {
 
 		// initialize config options:
 		// ...
+	}
+
+
+	@EventHandler
+	public void onPlayerJoinEvent(PlayerJoinEvent event) {
+		player = event.getPlayer();
+
+		new BukkitRunnable() {
+			public void run() {
+
+				GameMode gameMode = GameModeFlagScanner.getFinalGameMode(player);
+
+				if (gameMode != null) {
+					player.setGameMode(gameMode);
+					plugin.getLogger().info("Gamemode change in world " + player.getLocation().getWorld() + " for player " + player.getName() + ": new GameMode is now " + gameMode);
+				} else {
+					plugin.getLogger().info("No gamemode change");
+				}
+
+			}
+		}.runTaskLater(plugin, 20);
 	}
 
 
